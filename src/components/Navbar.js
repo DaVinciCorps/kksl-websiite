435a5cba161ff861c39ebcfec982874598524ee2
Utkarsh Koushik <utkarshkoushik007@gmail.com>
2021-12-08 13:03:18 +0530

    Navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { makeStyles } from '@mui/styles';
+import TrendImage from '../images/Vector.png'
 
 const useStyles = makeStyles({
     root:{
-        width: '100%',
+        // width: '100%',
         height: '70px',
         backgroundColor: '#0953AA',
         display: 'flex',
         alignItems: 'center',
-        paddingLeft: '14.5%',
-        paddingRight: '14.5%'
+        justifyContent : 'space-around',
+        // paddingLeft: '14.5%',
+        // paddingRight: '14.5%'
     },
 
     navLinks:{
         display: 'flex',
-        marginRight: 51,
+        // marginRight: 51,
 
     },
 
@@ -34,7 +36,65 @@ const useStyles = makeStyles({
         backgroundColor: '#042349',
         height: '100%',
         width: 325,
-        marginRight: 83,
+        // marginRight: 83,
+        color: '#FFFFFF',
+        justifyContent:'space-evenly',
+        display: 'flex',
+        flexDirection:'column'
+        // position: 'absolute',
+        // left: 722,
+        
+    },
+
+    shares : {
+        display:'flex',
+        flexDirection: 'row',
+        alignSelf:'center',
+    },
+
+    name :{
+        // paddingLeft:8,
+        paddingRight:6,
+        fontFamily: 'Rubik',
+        fontSize: '14px',
+        fontWeight: 'normal',
+        fontStyle: 'normal',
+        lineHeight:"18px",
+    },
+
+    value : {
+        paddingLeft:6,
+        paddingRight:6,
+        fontFamily: 'Rubik',
+        fontSize: '14px',
+        fontWeight: 'bold',
+        fontStyle: 'normal',
+        lineHeight:"18px",
+    },
+
+    trend : {
+        paddingLeft:6,
+        // paddingRight:8,
+        fontFamily: 'Rubik',
+        fontSize: '12px',
+        fontWeight: 'normal',
+        fontStyle: 'normal',
+        lineHeight:"21px",
+    },
+
+    signInButton : {
+        width: 102, 
+        color:"#FFFFFF", 
+        height: 40,
+        borderRadius: 8, 
+        borderWidth:2, 
+        borderColor:"#2584F4", 
+        backgroundColor:"#2584F4", 
+        display: 'flex', 
+        flexDirection: 'row', 
+        justifyContent: 'center', 
+        alignItems: "center", 
+        padding: '11 24'
     }
 
   });
@@ -61,12 +121,33 @@ export default function Navbar() {
             </div>
 
             <div className={classes.nifty}>
-
+                <div className={classes.shares}>
+                    <div className={classes.name}>
+                        NIFTY
+                    </div>
+                    <div className={classes.value}>
+                        15784.10
+                    </div>
+                    <div className={classes.trend}>
+                        <img src={TrendImage} alt="Trend Icon" style={{width: '15.67px', height: '13.58px'}} /> 46.35(0.29%)
+                    </div>
+                </div>
+                <div className={classes.shares}>
+                    <div className={classes.name}>
+                        SENSEX
+                    </div>
+                    <div className={classes.value}>
+                        52.515.51
+                    </div>
+                    <div className={classes.trend}>
+                        <img src={TrendImage} alt="Trend Icon" style={{width: '15.67px', height: '13.58px'}}/> 215.04(0.41%)
+                    </div>
+                </div>
             </div>
 
             <div >
-                <button style={{width: 102, height: 40,borderRadius: 8, back}}>
-
+                <button className={classes.signInButton}>
+                    Sign In
                 </button>
             </div>
         </div>
--- src/screen/HomePage.js
+++ src/screen/HomePage.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Navbar from '../components/Navbar'
+import Header from '../components/Header'
+import Home from '../components/Home'
+
 
 export default function HomePage() {
+    const [value, setValue] = useState(0);
     return (
         <div>
             <Navbar />
+            <Header value={0} />
+            <Home />
         </div>
     )
 }
